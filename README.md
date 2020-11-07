7d1d4fb45684dd901181c4ae75d1cd2e4d6f9dcf
Stephan Dilly <dilly.stephan@gmail.com>
2020-11-07 13:03:02 +0100

    more docs

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # fb-rs
 Facebook Graph API for Rust
+
+Supported endpoints:
+
+* `/me`
+* `/me/friends`
+* `/me/picture`
+
+Example:
+
+```rust
+let api:GraphAPI = FBGraphAPI::default();
+
+let me = api.me("<CLIENT_TOKEN>").await?;
+println!(
+    "me:\n id:\t{}\n name:\t{}",
+    me.id.unwrap(),
+    me.name.unwrap()
+);
+```
+
+prints:
+```
+me:
+ id:    <fbid>
+ name:  <your fb name>
+```
--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,19 @@ use std::env;
 #[tokio::main]
 async fn main() -> Result<()> {
     let token = env::var("ACCESS_TOKEN").unwrap_or_default();
-    let me = FBGraphAPI::default().me(&token).await?;
-    println!("me:\n{:#?}", me);
+    let api = FBGraphAPI::default();
 
-    let friends = FBGraphAPI::default().friends(&token).await?;
+    let me = api.me(&token).await?;
+    println!(
+        "me:\n id:\t{}\n name:\t{}",
+        me.id.unwrap(),
+        me.name.unwrap()
+    );
+
+    let friends = api.friends(&token).await?;
     println!("friends:\n{:#?}", friends.data);
 
-    let pic = FBGraphAPI::default().my_picture(&token).await?;
+    let pic = api.my_picture(&token).await?;
     println!("pic:\n{:#?}", pic.data);
 
     Ok(())
